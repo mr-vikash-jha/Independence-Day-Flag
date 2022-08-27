e408438ff0a455ea0576a842020f3ace30f8f727
unknown <vjha@medtigo.com>
2022-08-27 18:49:40 +0530

    basic changes

--- App.tsx
+++ App.tsx
@@ -67,3 +67,39 @@ const IndependenceDayScreen = () => {
 export default IndependenceDayScreen;
 
 const styles = StyleSheet.create({});
+
+
+// import React from 'react';
+// import { Text, View, StyleSheet, Image, Dimensions } from 'react-native';
+
+// let { width } = Dimensions.get('window');
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//     <Image
+//       source={require("./src/assets/images/card.png")}
+//       resizeMode="cover"
+//       style={styles.image}
+//     />
+//     <View style={{flexDirection:'row', justifyContent:'space-around',marginTop:6}}>
+//       <Text style ={{fontSize:15, color:' #2F2F2F',fontFamily:'Brandon Grotesque',lineHeight: 21 }}>
+//       ABSTRACT PRINT MODAL MINI DRESS
+//       </Text>
+//       <Text style ={{fontSize:15, color:' #2F2F2F',fontFamily:'Brandon Grotesque',lineHeight: 21 }}>
+//       Rs. 2499
+//       </Text>
+//     </View>
+//   </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//   },
+//   image: {
+//     width: 429,
+//     height: 532,
+//   }
+// });
