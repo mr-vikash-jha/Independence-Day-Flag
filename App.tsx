70fc7ffc2b16bb45a82057deea30b0e606d60270
unknown <vjha@medtigo.com>
2022-08-27 16:25:11 +0530

    initial commit

--- App.tsx
+++ App.tsx
@@ -8,6 +8,8 @@ const IndependenceDayScreen = () => {
     0, 15, 30, 45, 60, 75, 90, 105, 120, 135, 150, 165, 180, 195, 210, 225,
   ]);
 
+  
+
   return (
     <View style={{flex: 1}}>
       <View style={{flex: 1, backgroundColor: '#FF9933'}}></View>
